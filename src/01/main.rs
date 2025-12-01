f9c78106fb97fdd92a2c49a48474e7e1875aa7c1
Jordi Been <jordi.been@ridecheck.app>
2025-12-01 13:34:40 +0100

    day 01 part 2

--- src/01/main.rs
+++ src/01/main.rs
@@ -6,6 +6,7 @@ fn main() {
 
     let input: &str = include_str!("./input.txt");
     println!("pt1: {} (finished in {:.2?})", pt1(&input), start.elapsed());
+    println!("pt2: {} (finished in {:.2?})", pt2(&input), start.elapsed());
 }
 
 fn pt1(input: &str) -> i32 {
@@ -14,9 +15,6 @@ fn pt1(input: &str) -> i32 {
         .lines()
         .filter(|x| {
             let (direction, distance) = x.split_at(1);
-            println!("dial: {:?}", dial);
-            println!("dir: {:?}", direction);
-            println!("distance: {:?}", distance);
             match direction {
                 "L" => {
                     dial -= distance.parse::<i32>().unwrap();
@@ -31,14 +29,41 @@ fn pt1(input: &str) -> i32 {
         .count() as i32
 }
 
+fn pt2(input: &str) -> i32 {
+    let mut dial: i32 = 50;
+    input
+        .lines()
+        .map(|x| {
+            let (direction, distance) = x.split_at(1);
+            (0..distance.parse::<i32>().unwrap())
+                .filter(|_| {
+                    match direction {
+                        "L" => dial -= 1,
+                        "R" => dial += 1,
+                        _ => unreachable!(),
+                    };
+                    dial % 100 == 0
+                })
+                .count() as i32
+        })
+        .sum()
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
     fn pt1_test() {
-        let input = include_str!("./example_pt1.txt");
+        let input = include_str!("./example.txt");
         let result = pt1(&input);
         assert_eq!(result, 3);
+    }
+
+    #[test]
+    fn pt2_test() {
+        let input = include_str!("./example.txt");
+        let result = pt2(&input);
+        assert_eq!(result, 6);
     }
 }
