e19c818cd9e88ecaa848bd78e9d42a6e725e52a4
Jordi Been <jordi.been@ridecheck.app>
2025-12-02 13:05:16 +0100

    day 02 part 2

--- src/02/main.rs
+++ src/02/main.rs
@@ -1,12 +1,13 @@
-use core::num;
+use std::collections::HashSet;
 use std::time::Instant;
 
 fn main() {
-    println!("--- Day 1: Secret Entrance ---");
+    println!("--- Day 2: Gift Shop ---");
     let start: Instant = Instant::now();
 
     let input: &str = include_str!("./input.txt");
     println!("pt1: {} (finished in {:.2?})", pt1(&input), start.elapsed());
+    println!("pt2: {} (finished in {:.2?})", pt2(&input), start.elapsed());
 }
 
 fn pt1(input: &str) -> i64 {
@@ -29,6 +30,32 @@ fn pt1(input: &str) -> i64 {
     sum as i64
 }
 
+fn pt2(input: &str) -> i64 {
+    let mut sum = 0;
+    input.split(",").for_each(|range| {
+        let (start, end) = range
+            .trim()
+            .split_once("-")
+            .map(|s| (s.0.parse::<i64>().unwrap(), s.1.parse::<i64>().unwrap()))
+            .unwrap();
+
+        (start..end + 1).for_each(|num| {
+            let chars = num.to_string().chars().collect::<Vec<char>>();
+            for i in 1..chars.len() {
+                let set = chars
+                    .chunks(i)
+                    .map(|chunk| chunk.iter().collect::<String>())
+                    .collect::<HashSet<String>>();
+                if set.len() == 1 {
+                    sum += num;
+                    break;
+                }
+            }
+        });
+    });
+    sum as i64
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -38,5 +65,12 @@ mod tests {
         let input = include_str!("./example.txt");
         let result = pt1(&input);
         assert_eq!(result, 1227775554);
+    }
+
+    #[test]
+    fn pt2_test() {
+        let input = include_str!("./example.txt");
+        let result = pt2(&input);
+        assert_eq!(result, 4174379265);
     }
 }
