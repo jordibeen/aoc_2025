3517f0ceb4686552a44cfe0d855132b5623a0a09
Jordi Been <jordi.been@ridecheck.app>
2025-12-04 13:52:37 +0100

    day 04 part 2

--- src/04/main.rs
+++ src/04/main.rs
@@ -1,3 +1,4 @@
+use std::i32;
 use std::time::Instant;
 
 fn main() {
@@ -5,26 +6,48 @@ fn main() {
     let start: Instant = Instant::now();
 
     let input: &str = include_str!("./input.txt");
-    println!("pt1: {}", pt1(&input));
+    let grid = parse(&input);
+    println!("pt1: {}", pt1(grid.clone()));
+    println!("pt2: {}", pt2(grid));
     println!("Execution time: {:.2?}", start.elapsed());
 }
 
-fn pt1(input: &str) -> i32 {
-    let grid = input
+fn parse(input: &str) -> Vec<Vec<char>> {
+    input
         .lines()
         .map(|line| line.chars().collect::<Vec<char>>())
-        .collect::<Vec<Vec<char>>>();
+        .collect()
+}
 
+fn pt1(grid: Vec<Vec<char>>) -> i32 {
+    let (_, removal_count) = remove_accessible_rolls(grid);
+    removal_count
+}
+
+fn pt2(mut grid: Vec<Vec<char>>) -> i32 {
+    let mut ans = 0;
+
+    let mut removal_count = i32::MAX;
+    while removal_count != 0 {
+        (grid, removal_count) = remove_accessible_rolls(grid);
+        ans += removal_count
+    }
+
+    ans
+}
+
+fn remove_accessible_rolls(mut grid: Vec<Vec<char>>) -> (Vec<Vec<char>>, i32) {
     let n = grid.len();
     let m = grid[0].len();
 
-    grid.iter()
+    let removals: Vec<(usize, usize)> = grid
+        .iter()
         .enumerate()
-        .map(|(y, row)| {
+        .flat_map(|(y, row)| {
             row.iter()
                 .enumerate()
-                .filter(|(x, c)| {
-                    if **c == '@' {
+                .filter_map(|(x, c)| {
+                    if *c == '@' {
                         let adjacent_roll_count = [
                             (-1, -1),
                             (-1, 0),
@@ -38,21 +61,28 @@ fn pt1(input: &str) -> i32 {
                         .iter()
                         .filter(|dir| {
                             let yy = y as isize + dir.0;
-                            let xx = *x as isize + dir.1;
+                            let xx = x as isize + dir.1;
                             (yy >= 0 && yy < n as isize)
                                 && (xx >= 0 && xx < m as isize)
                                 && grid[yy as usize][xx as usize] == '@'
                         })
                         .count();
 
-                        adjacent_roll_count < 4
-                    } else {
-                        false
-                    }
+                        if adjacent_roll_count < 4 {
+                            return Some((y, x));
+                        };
+                    };
+                    None
                 })
-                .count() as i32
+                .collect::<Vec<(usize, usize)>>()
         })
-        .sum()
+        .collect();
+
+    removals.iter().for_each(|(y, x)| {
+        grid[*y as usize][*x as usize] = '.';
+    });
+
+    (grid, removals.len() as i32)
 }
 
 #[cfg(test)]
@@ -62,7 +92,16 @@ mod tests {
     #[test]
     fn pt1_test() {
         let input = include_str!("./example.txt");
-        let result = pt1(&input);
+        let grid = parse(&input);
+        let result = pt1(grid);
         assert_eq!(result, 13);
+    }
+
+    #[test]
+    fn pt2_test() {
+        let input = include_str!("./example.txt");
+        let grid = parse(&input);
+        let result = pt2(grid);
+        assert_eq!(result, 43);
     }
 }
