7f0b8f6a05a06c6da360260c9c45da64c0b0c50b
Jordi Been <jordi.been@ridecheck.app>
2025-12-07 18:00:23 +0100

    day 07 part 2

--- src/07/main.rs
+++ src/07/main.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::time::Instant;
 
 fn main() {
@@ -6,6 +7,7 @@ fn main() {
 
     let input: &str = include_str!("./input.txt");
     println!("pt1: {}", pt1(&input));
+    println!("pt2: {}", pt2(&input));
     println!("Execution time: {:.2?}", start.elapsed());
 }
 
@@ -34,6 +36,53 @@ fn pt1(input: &str) -> i32 {
     splits
 }
 
+fn pt2(input: &str) -> i64 {
+    let mut starting_pos = (0, 0);
+    let grid = input
+        .lines()
+        .enumerate()
+        .map(|(y, row)| {
+            row.chars()
+                .enumerate()
+                .map(|(x, c)| {
+                    if c == 'S' {
+                        starting_pos = (y, x);
+                    }
+                    c
+                })
+                .collect::<Vec<char>>()
+        })
+        .collect::<Vec<Vec<char>>>();
+
+    ways(&mut HashMap::new(), &grid, starting_pos)
+}
+
+fn ways(
+    cache: &mut HashMap<(usize, usize), i64>,
+    grid: &Vec<Vec<char>>,
+    pos: (usize, usize),
+) -> i64 {
+    if let Some(cached_val) = cache.get(&pos) {
+        return *cached_val;
+    }
+
+    let mut timelines = 0;
+    if pos.0 + 1 == grid.len() - 1 {
+        timelines = 1;
+    } else {
+        if grid[pos.0 + 1][pos.1] == '^' {
+            timelines += ways(cache, grid, (pos.0 + 1, pos.1 - 1));
+            timelines += ways(cache, grid, (pos.0 + 1, pos.1 + 1));
+        } else {
+            timelines += ways(cache, grid, (pos.0 + 1, pos.1));
+        }
+    }
+
+    cache.insert(pos, timelines);
+
+    timelines
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -43,5 +92,12 @@ mod tests {
         let input = include_str!("./example.txt");
         let result = pt1(&input);
         assert_eq!(result, 21);
+    }
+
+    #[test]
+    fn pt2_test() {
+        let input = include_str!("./example.txt");
+        let result = pt2(&input);
+        assert_eq!(result, 40);
     }
 }
